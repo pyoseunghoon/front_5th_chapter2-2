b2381e1e8f804813abe4a7f350f3af110be10f43
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-22 15:50:58 +0900

    adjust prettier & eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
+// .eslintrc.cjs
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react-refresh',
+    'react-hooks',
+  ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+  ignorePatterns: ['dist', 'node_modules'],
+};
