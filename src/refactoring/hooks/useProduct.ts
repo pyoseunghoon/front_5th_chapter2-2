2b799c0c27748c1a70ba88e3a9ca013c625a0fa0
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-22 16:09:38 +0900

    Add useState, add, set func in useProduct.ts

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,22 @@
-import { useState } from "react";
-import { Product } from "../../types.ts";
+import { useState } from 'react';
+import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState(initialProducts);
+
+  // [관리자] - 상품 추가 기능
+  const addProduct = (product: Product) => {
+    setProducts((prev) => [...prev, product]);
+  };
+
+  // [관리자] - 상품 property 수정 기능
+  const updateProduct = (product: Product) => {
+    setProducts((prev) => prev.map((p) => (p.id === product.id ? product : p)));
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
