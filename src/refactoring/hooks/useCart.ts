a9552ce73a3bcabe45b8906f221d33f79d765e57
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-22 16:35:24 +0900

    Add addToCart func in useCart.ts

--- src/refactoring/hooks/useCart.ts
+++ src/refactoring/hooks/useCart.ts
@@ -1,13 +1,27 @@
 // useCart.ts
-import { useState } from "react";
-import { CartItem, Coupon, Product } from "../../types";
-import { calculateCartTotal, updateCartItemQuantity } from "../models/cart";
+import { useState } from 'react';
+import { CartItem, Coupon, Product } from '../../types';
+import { calculateCartTotal, updateCartItemQuantity } from '../models/cart';
 
 export const useCart = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
 
-  const addToCart = (product: Product) => {};
+  const addToCart = (product: Product) => {
+    const isInCart = cart.some((p) => p.product.id === product.id);
+
+    if (isInCart) {
+      // 기존에 장바구니에 있는 상품 추가
+      setCart((prev) =>
+        prev.map((p) =>
+          p.product.id === product.id ? { ...p, quantity: p.quantity + 1 } : p,
+        ),
+      );
+    } else {
+      // 기존에 장바구니에 없던 상품 추가
+      setCart((prev) => [...prev, { product, quantity: 1 }]);
+    }
+  };
 
   const removeFromCart = (productId: string) => {};
 
