a49c04a8a6fc5e8ce329cbe4c31bc769e045bd2d
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-22 16:14:49 +0900

    Add useState, add func in useCoupon.ts

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,12 @@
-import { useState } from "react";
-import { Coupon } from "../../types.ts";
+import { useState } from 'react';
+import { Coupon } from '../../types.ts';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState(initialCoupons);
+
+  const addCoupon = (coupon: Coupon) => {
+    setCoupons((prev) => [...prev, coupon]);
+  };
+
+  return { coupons, addCoupon };
 };
