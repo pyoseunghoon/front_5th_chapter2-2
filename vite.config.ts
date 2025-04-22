a6dfa0e1ff9f3204fa187411a84a7b39af1f5a9f
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-23 00:47:00 +0900

    배포 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: '/front_5th_chapter2-2/',
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.origin.html'),
+          refactoring: resolve(__dirname, 'index.refactoring.html'),
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
-  })
-)
+  }),
+);
