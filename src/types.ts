f564a0b2a764c65533e3ecd042e85dfe7f108cae
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-23 12:42:06 +0900

    discountType 리터럴 타입 추가

--- src/types.ts
+++ src/types.ts
@@ -16,9 +16,16 @@ export interface CartItem {
   quantity: number;
 }
 
+// discountType 리터럴 타입
+export const DISCOUNT_TYPE = {
+  AMOUNT: 'amount',
+  PERCENTAGE: 'percentage',
+} as const;
+export type DiscountType = (typeof DISCOUNT_TYPE)[keyof typeof DISCOUNT_TYPE];
+
 export interface Coupon {
   name: string;
   code: string;
-  discountType: 'amount' | 'percentage';
+  discountType: DiscountType;
   discountValue: number;
 }
